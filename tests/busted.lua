9f42eed507b3113f15be0fb80dae99c0fc23dec8
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-24 14:32:19 +0100

    ci: update test infrastructure

--- tests/busted.lua
+++ tests/busted.lua
@@ -1,3 +1,5 @@
+local language = "typescript"
+
 local lazypath = vim.env.LAZY_STDPATH
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
@@ -12,31 +14,47 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local root = vim.fn.fnamemodify(vim.env.LAZY_STDPATH, ":p")
-for _, name in ipairs({ "config", "data", "state", "cache" }) do
-    vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
+local treesitter_spec
+local lockfile
+if vim.fn.has("nvim-0.11") == 1 then
+    treesitter_spec = {
+        "nvim-treesitter/nvim-treesitter",
+        config = function(_, opts)
+            require("nvim-treesitter").setup(opts)
+            local installed = require("nvim-treesitter.config").get_installed()
+            if not vim.tbl_contains(installed, language) then
+                require("nvim-treesitter").install({ language }):wait()
+            end
+        end,
+    }
+    lockfile = "tests/lazy-lock.json"
+else
+    treesitter_spec = {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+            local parsers = require("nvim-treesitter.parsers")
+            if not parsers.has_parser(language) then vim.cmd("TSInstallSync " .. language) end
+        end,
+    }
+    lockfile = "tests/lazy-lock-10.json"
 end
 
-local opts = require("lazy.minit").busted.setup({
+local opts = {
     spec = {
         "nvim-lua/plenary.nvim",
-        {
-            "nvim-treesitter/nvim-treesitter",
-            build = ":TSUpdate",
-            main = "nvim-treesitter.configs",
-            opts = { ensure_installed = { "typescript" } },
-        },
+        treesitter_spec,
         "nvim-neotest/nvim-nio",
         "nvim-neotest/neotest",
     },
-    lockfile = "tests/lazy-lock.json",
-})
-
-vim.o.loadplugins = true
-require("lazy").setup(opts)
+    lockfile = lockfile,
+}
 
 if _G.arg[1] == "--update" then
-    require("lazy").update():wait()
+    table.remove(_G.arg, 1)
+    require("lazy.minit").setup(opts)
 else
-    require("lazy.minit").busted.run()
+    table.insert(_G.arg, "--offline")
+    require("lazy.minit").busted(opts)
 end
